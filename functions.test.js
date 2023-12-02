0fa410138772794ab7f989f0c56d6c8f853e5689
Dm168 <vpoliakin@mail.ru>
2023-12-02 15:34:28 +0500

    edit json

--- functions.test.js
+++ functions.test.js
@@ -10,6 +10,8 @@ test(' Checking the number of words in the text ', () => {
   expect(getWordCount('!tt ,aa _ rr ', 'rr')).toBe(1);
   expect(getWordCount('!tt ,aa _rr ', 'r')).toBe(1);
   expect(getWordCount('!tt ,aa _rr Br ', 'br')).toBe(0);
+  expect(getWordCount('!tt ,aa _rr Br ', 'br')).toBe(0);
+
 });
 
 
