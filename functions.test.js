e81ffa2fb9d383a0c28e19ccac971c60cf83ae99
Dm168 <vpoliakin@mail.ru>
2023-12-02 15:11:56 +0500

    test setup for the function

--- functions.test.js
+++ functions.test.js
@@ -1,12 +1,15 @@
 
 import getWordCount from './functions/wordAndText.js';
 
-import getWordCount from './functions/wordAndText.js';
 
 
 
-test(' to equal 3', () => {
-  expect(add(1, 2)).toBe(3);
+
+test(' Checking the number of words in the text ', () => {
+  expect(getWordCount('!tt ,aa _rr AA ', 'aa')).toBe(1);
+  expect(getWordCount('!tt ,aa _ rr ', 'rr')).toBe(1);
+  expect(getWordCount('!tt ,aa _rr ', 'r')).toBe(1);
+  expect(getWordCount('!tt ,aa _rr Br ', 'br')).toBe(0);
 });
 
 
--- functions/wordAndText.js
+++ functions/wordAndText.js
@@ -1,17 +1,23 @@
 
-
-
-
 const getWordCount = (text, word) => {
   let result = 0;
-  text = text.split(' ')
-  for (let i = 0; i < text.length; i++) {
-    if (text[i].indexOf(word) === 0) { result += 1 }
+  let arrayTranslation = text.replace(/[-_.,%?!]/g, '').split(' ');
+  for (let i = 0; i < arrayTranslation.length; i++) {
+    if (arrayTranslation[i].indexOf(word) === 0) { result += 1 }
   };
   return result
 }
 
 export default getWordCount;
+
+
+
+
+
+
+
+
+
 
 
 
