5b16340ad5bf6c155371b5ffc6c090a4c6dc15a9
Dm168 <vpoliakin@mail.ru>
2023-12-04 08:45:12 +0500

    function and test editing

--- functions.test.js
+++ functions.test.js
@@ -1,17 +1,23 @@
 
 import getWordCount from './functions/wordAndText.js';
 
-
-
-
-
 test(' Checking the number of words in the text ', () => {
-  expect(getWordCount('!tt ,aa _rr AA ', 'aa')).toBe(1);
-  expect(getWordCount('!tt ,aa _ rr ', 'rr')).toBe(1);
-  expect(getWordCount('!tt ,aa _rr ', 'r')).toBe(1);
-  expect(getWordCount('!tt ,aa _rr Br ', 'br')).toBe(0);
-  expect(getWordCount('!tt ,aa _rr Br ', 'br')).toBe(0);
-
+  expect(getWordCount('Lorem ipsum dolor sit amet', 'sit')).toBe(1);
+  expect(getWordCount('Lorem ipsum ,dolor? sit amet', 'dolor')).toBe(1);
+  expect(getWordCount('Lorem ipsum dolor sit am,et', 'amet')).toBe(0);
+  expect(getWordCount('Lorem ipsum dolor sit amet', 'LoreM')).toBe(0);
+  expect(getWordCount('Lorem ipsum dolor sit amet', 'd o l o r')).toBe(0);
+  expect(getWordCount('Lorem ipsum dolor sit amet', 'ruble')).toBe(0);
+  expect(getWordCount('Lorem ipsum dolor sit amet', ' ')).toBe(0);
+  expect(getWordCount('Lorem ipsum dolor sit! amet', 'sit')).toBe(1);
+  expect(getWordCount('Lorem  Lorem ,   Lorem ', 'Lorem')).toBe(3);
+  expect(getWordCount('Lorem ipsum dolor sit amet , sit', 'sit')).toBe(2);
 });
+
+
+
+
+
+
 
 
--- functions/wordAndText.js
+++ functions/wordAndText.js
@@ -1,11 +1,11 @@
 
 const getWordCount = (text, word) => {
   let result = 0;
-  let arrayTranslation = text.replace(/[-_.,%?!]/g, '').split(' ');
+  let arrayTranslation = text.replace(/[-_.,%?!]/g, ' ').split(' ');
   for (let i = 0; i < arrayTranslation.length; i++) {
-    if (arrayTranslation[i].indexOf(word) === 0) { result += 1 }
-  };
-  return result
+    if (arrayTranslation[i] === word) { result += 1 }
+
+  }; return result
 }
 
 export default getWordCount;
