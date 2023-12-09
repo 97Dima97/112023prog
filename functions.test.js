ed47e0979c1792dd4744688b206fac5dcc5821a1
Dm168 <vpoliakin@mail.ru>
2023-12-09 15:06:17 +0500

    changed the test and the mockdate

--- functions.test.js
+++ functions.test.js
@@ -3,6 +3,9 @@ import getWordCount from './functions/getWordCount.js';
 
 import getTimeUtc from './functions/getTimeUtc.js';
 
+import MockDate from 'mockdate'
+
+
 test(' Checking the number of words in the text ', () => {
   expect(getWordCount('Lorem ipsum dolor ,sit amet , sit amet ', 'sit')).toBe(2);
   expect(getWordCount('Lorem ipsum dolor sit amet', 'ruble')).toBe(0);
@@ -11,12 +14,12 @@ test(' Checking the number of words in the text ', () => {
 });
 
 
-
+MockDate.set(2012, 1, 20, 1, 12)
 
 test('See the time in the time zone ', () => {
-  expect(getTimeUtc(-7, false)).toBe(`a.m. ${new Date(2012, 1, 20, 3, 12).getHours() - 3}:${new Date().getMinutes()}`);
-  expect(getTimeUtc(+10, false)).toBe(`a.m. ${new Date(2012, 1, 20, 3, 12).getHours() + 2}:${new Date().getMinutes()}`);
-  expect(getTimeUtc(0,)).toBe(`p.m. ${new Date(2012, 1, 20, 13, 12).getHours() + 6}:${new Date().getMinutes()}`);
+  expect(getTimeUtc(-7, false)).toBe(`a.m. ${new Date(2012, 1, 20, 3, 12).getHours() + 2}:0${new Date().getMinutes()}`);
+  expect(getTimeUtc(+10, false)).toBe(`a.m. ${new Date(2012, 1, 20, 3, 12).getHours() + 7}:0${new Date().getMinutes()}`);
+  expect(getTimeUtc(0,)).toBe(`p.m. ${new Date(2012, 1, 20, 13, 12).getHours() - 1}:0${new Date().getMinutes()}`);
   expect(getTimeUtc(1.1,)).toBe('unavailable value');
   expect(getTimeUtc(15,)).toBe('unavailable value');
   expect(getTimeUtc(-15,)).toBe('unavailable value');
