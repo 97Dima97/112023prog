65768eafd29e694046386bd3a5c9c53ead73c9b4
Dm168 <vpoliakin@mail.ru>
2023-12-31 12:10:41 +0500

    edited the test function

--- function.test/getTimeUtcString.test.js
+++ function.test/getTimeUtcString.test.js
@@ -1,16 +1,12 @@
 import getTimeUtcString from '../functions/getTimeUtcString.js';
 import MockDate from 'mockdate';
 
-const аfternoonTime = new Date(2012, 1, 20, 13, 0);
-const midnightTime = new Date(2012, 1, 20, 0, 0);
-
 test('See the time in the time zone ', () => {
     MockDate.set(2012, 1, 20, 1, 12);
     expect(getTimeUtcString(-7, false)).toBe(`5:00`);
     expect(getTimeUtcString(+10, false)).toBe(`10:00`);
-    expect(getTimeUtcString(0)).toBe(`${аfternoonTime.getHours() - 13}:0${аfternoonTime.getMinutes()}`);
-    expect(getTimeUtcString(0)).toBe(`${midnightTime.getHours()}:0${midnightTime.getMinutes()}`);
     expect(getTimeUtcString()).toBe(`0:00`);
+    expect(getTimeUtcString(0)).toBe(`0:00`);
 
     MockDate.reset();
 });
@@ -23,5 +19,6 @@ test('Checking for invalid values', () => {
     expect(getTimeUtcString => getTimeUtcString(-14))
         .toThrow();
 });
+
 
 
